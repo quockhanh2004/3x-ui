f362cb5ff1718350976c64a1df72a687b28110da
Phạm Ngọc Quốc Khánh <phamngocquockhanh2004@gmail.com>
2025-04-09 12:04:35 +0700

    update script

--- x-ui.sh
+++ x-ui.sh
@@ -519,7 +519,7 @@ enable_bbr() {
 }
 
 update_shell() {
-    wget -O /usr/bin/x-ui -N https://github.com/MHSanaei/3x-ui/raw/main/x-ui.sh
+    wget -O /usr/bin/x-ui -N https://github.com/quockhanh2004/3x-ui/raw/main/x-ui.sh
     if [[ $? != 0 ]]; then
         echo ""
         LOGE "Failed to download script, Please check whether the machine can connect Github"
